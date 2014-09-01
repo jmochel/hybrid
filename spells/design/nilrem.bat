ee1261d6ba9dbc2b371984e3e5a96f5295178e62
jmochel <jmochel@saltations.org>
2014-08-31 22:24:05 -0400

    Removed duplicate files

--- spells/design/nilrem.bat
+++
@@ -1,5 +0,0 @@
-awk -f spelldes.awk augury.des     > nilrem.txt
-awk -f spelldes.awk force.des      >> nilrem.txt
-awk -f spelldes.awk kwikdraw.des   >> nilrem.txt
-
-
--- spells/koreal.bat
+++
@@ -1,4 +0,0 @@
-awk -f spelldes.awk kwikdraw.des > koreal.txt
-awk -f spelldes.awk laser.des  >> koreal.txt
-
-
